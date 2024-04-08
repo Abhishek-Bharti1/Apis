32953f3e5c79b46836c646755bb332e394e42373
Abhishek Bharti <abhishekbharti91550@gmail.com>
2024-04-08 09:35:55 +0530

    fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors())
 const PORT =process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 // Create a schema for tasks
